26906f23dcf58e2eace501489183ed9d86eb3acc
zabihullahburhani <110895951+zabihullahburhani@users.noreply.github.com>
2025-07-29 20:46:47 +0430

    deleted
    the previus files have been deleted by burhani

--- app/models/gold_type_model.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, Text
-from .db import Base
-
-class GoldType(Base):
-    __tablename__ = "gold_types"
-
-    gold_type_id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    description = Column(Text)
--- app/routes/admin.py
+++
@@ -1,9 +0,0 @@
-# app/routes/admin.py
-from fastapi import APIRouter, Depends
-from app.blockchain.blockchain import blockchain
-
-router = APIRouter()
-
-@router.get("/logs")
-async def get_logs():
-    return blockchain.chain
